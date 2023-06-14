c478f30441de6c98cf3f8cef9a1f72ebe9e595ed
Liliyaa11 <120708561+Liliyaa11@users.noreply.github.com>
2023-06-14 13:22:28 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,20 +1,36 @@
-﻿namespace Mladejki_Centur_OOP
+﻿using System.ComponentModel;
+
+namespace Mladejki_Centur_OOP
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            try { 
+            Console.Write("Брой младежи: ");
             int n = int.Parse(Console.ReadLine());
             List<Mladej> mladej=new List<Mladej> ();
-            try
-            {
+
                 //Въвежда данни за младежите в дадена организация
-                Console.Write("Брой младежи: ");
+                Console.WriteLine("vavedi 1-za uchenik, 2-student, 3-rabotnik");
+                int nomer = int.Parse(Console.ReadLine());
+                switch(n)
+                {
+                    case 1:
                 for (int i = 0; i < n; i++)
                 {
-                    Console.WriteLine($"Младеж: {mladej}");
-                    Console.Write("Име: ");
-                }
+                    var danni = Console.ReadLine().Split();
+                    Mladej uchenik=new Uchenik(danni[0], danni[1], danni[2], int.Parse(danni[3]));
+                   mladej.Add(uchenik);
+                } break;
+                    case 2:
+                        for (int i = 0; i < n; i++)
+                        {
+                            var danni = Console.ReadLine().Split();
+                            Mladej student = new Uchenik(danni[0], danni[1], danni[2], int.Parse(danni[3]));
+                            mladej.Add(uchenik);
+                        }
+                        break;
             }
             catch (Exception)
             {
