aedc097f366d33676fef78ac050a497a8186642c
Liliyaa11 <120708561+Liliyaa11@users.noreply.github.com>
2023-06-21 13:16:48 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -6,36 +6,48 @@ namespace Mladejki_Centur_OOP
     {
         static void Main(string[] args)
         {
-            try { 
-            Console.Write("Брой младежи: ");
-            int n = int.Parse(Console.ReadLine());
-            List<Mladej> mladej=new List<Mladej> ();
+            try
+            {
+                Console.Write("Брой младежи: ");
+                int n = int.Parse(Console.ReadLine());
+                List<Mladej> mladej = new List<Mladej>();
 
                 //Въвежда данни за младежите в дадена организация
                 Console.WriteLine("vavedi 1-za uchenik, 2-student, 3-rabotnik");
                 int nomer = int.Parse(Console.ReadLine());
-                switch(n)
+                switch (n)
                 {
                     case 1:
-                for (int i = 0; i < n; i++)
-                {
-                    var danni = Console.ReadLine().Split();
-                    Mladej uchenik=new Uchenik(danni[0], danni[1], danni[2], int.Parse(danni[3]));
-                   mladej.Add(uchenik);
-                } break;
+                        for (int i = 0; i < n; i++)
+                        {
+                            var danni = Console.ReadLine().Split();
+                            Mladej uchenik = new Uchenik(danni[0], danni[1], danni[2], int.Parse(danni[3]));
+                            mladej.Add(uchenik);
+                        }
+                        break;
                     case 2:
                         for (int i = 0; i < n; i++)
                         {
                             var danni = Console.ReadLine().Split();
-                            Mladej student = new Uchenik(danni[0], danni[1], danni[2], int.Parse(danni[3]));
-                            mladej.Add(uchenik);
+                            Mladej student = new Student(danni[0], danni[1], danni[2], danni[3], int.Parse(danni[4]));
+                            mladej.Add(student);
                         }
                         break;
+                    case 3:
+                        for (int i = 0; i < n; i++)
+                        {
+                            var danni = Console.ReadLine().Split();
+                            Mladej rabotnik = new Rabota(danni[0], danni[1], danni[2], danni[3], danni[4], int.Parse(danni[5]));
+                            mladej.Add(rabotnik);
+                        }
+                        break;
+                }
             }
+
             catch (Exception)
             {
 
-                throw;
+                
             }
 
         }
